14d2eaf890135a28dea8e62ecabd82b2bbcabe2e
jakob.gazic <jakob.gazic@flintmail.com>
2022-07-03 12:42:20 +0200

    successfully storing data

--- app.py
+++ app.py
@@ -25,26 +25,29 @@ def create_table():
     )
 
     mycursor = mydb.cursor()
-    mycursor.execute("CREATE TABLE orderbook (exchange CHAR(20), bid DECIMAL(10,4), ask DECIMAL(10,4), timestamp DATETIME)")
-#create_database()
-#create_table()
+    mycursor.execute("CREATE TABLE orderbook (exchange CHAR(20), bid FLOAT(10,4), ask FLOAT(10,4), diff_bid FLOAT(10,4), diff_ask FLOAT(10,4), timestamp DATETIME)")
 
-# this needs to be another thread
-def display_data():
-    mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="password",
-    database="ccxtdatabase"
-    )
+def get_orderbook():
+    exchange1 = ccxt.binance()
+    exchange2 = ccxt.kucoin()
 
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT bid, exchange  FROM orderbook WHERE timestamp >= DATE_SUB(NOW(),INTERVAL 1 MINUTE)")
+    orderbook1 = exchange1.fetch_order_book('BTC/USDT')
+    orderbook2 = exchange2.fetch_order_book('BTC/USDT')
 
-    myresult = mycursor.fetchall()
-    print (json.dumps(str(myresult),indent=4),) 
+    dict1 = orderbook1
+    highest_bid1 = dict1.get('bids')[0][0]
+    lowest_ask1 = dict1.get('asks')[0][0]
 
-def save_data(exchange1,bid1,ask1,exchange2,bid2,ask2):
+    dict2 = orderbook2
+    highest_bid2 = dict2.get('bids')[0][0]
+    lowest_ask2 = dict2.get('asks')[0][0]
+    
+    diff_bid = highest_bid1 - highest_bid2
+    diff_ask = lowest_ask1 - lowest_ask2
+    print(diff_bid)
+    save_data(f"{exchange1.id}", highest_bid1,lowest_ask1,f"{exchange2.id}",highest_bid1,lowest_ask2, diff_bid, diff_ask)
+
+def save_data(exchange1,bid1,ask1,exchange2,bid2,ask2, diff_bid, diff_ask):
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -55,44 +58,20 @@ def save_data(exchange1,bid1,ask1,exchange2,bid2,ask2):
     mycursor = mydb.cursor()
 
     now = datetime.now()
-    id = 1
     formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
-    sql = "INSERT INTO orderbook (exchange, bid, ask, timestamp) VALUES (%s, %s, %s, %s)"
+    sql = "INSERT INTO orderbook (exchange, bid, ask, diff_bid, diff_ask, timestamp) VALUES (%s, %s, %s, %s,%s,%s)"
     val = [
-  (exchange1, bid1, ask1, formatted_date),
-  (exchange2, bid2, ask2, formatted_date),
+  (exchange1, bid1, ask1, diff_bid, diff_ask, formatted_date),
+  (exchange2, bid2, ask2, diff_bid, diff_ask, formatted_date),
 ]
     mycursor.executemany(sql, val)
     mydb.commit()
     print('saved')
     print(formatted_date)
 
-def get_orderbook():
-    while True:
-        exchange1 = ccxt.binance()
-        exchange2 = ccxt.kucoin()
-
-        orderbook1 = exchange1.fetch_order_book('BTC/USDT')
-        orderbook2 = exchange2.fetch_order_book('BTC/USDT')
-
-        dict1 = orderbook1
-        highest_bid1 = dict1.get('bids')[0]
-        lowest_ask1 = dict1.get('asks')[0]
-
-        dict2 = orderbook2
-        highest_bid2 = dict2.get('bids')[0]
-        lowest_ask2 = dict2.get('asks')[0]
-        
-        diff_bid = highest_bid1[0] - highest_bid2[0]
-        now = datetime.now()
-        
-        print(f'{exchange1}\n bid:{highest_bid1[0]}\n quantity: {highest_bid1[1]}\n ask:{lowest_ask1[0]}\n quantity: {lowest_ask1[1]}\n ')
-        print(f'{exchange2}\n bid:{highest_bid2[0]}\n quantity: {highest_bid2[1]}\n ask:{lowest_ask2[0]}\n quantity: {lowest_ask2[1]}\n ')
-        print(diff_bid)
-        print(now)
-
-        #modify database to also store diff, this does not affect performance much
-
-get_orderbook()
+#create_database()
+#create_table()
+while True:
+    get_orderbook()
 
 
