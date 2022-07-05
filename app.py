43b42aa4481e21b836d3ba278616ed38ebf6acdb
jakob.gazic <jakob.gazic@flintmail.com>
2022-07-05 15:21:55 +0200

    Change how to display data

--- app.py
+++ app.py
@@ -2,61 +2,67 @@ import ccxt
 import mysql.connector
 from datetime import datetime 
 import time
-import pandas as pd
-import numpy as np 
+import credentials
 
+username = credentials.username
+password = credentials.password
 
 def create_database():
-    mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="password",
-    )
+    try:
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user=f"{username}",
+        password=f"{password}",
+        )
 
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE DATABASE ccxtdatabase")
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE ccxtdatabase")
+    except:
+        return False
 
 def create_table():
-    mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="password",
-    database="ccxtdatabase"
-    )
+    try:
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user=f"{username}",
+        password=f"{password}",
+        database="ccxtdatabase"
+        )
 
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE TABLE orderbook (exchange CHAR(20), bid FLOAT(10,4), ask FLOAT(10,4), diff_bid FLOAT(10,4), diff_ask FLOAT(10,4), timestamp DATETIME)")
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE TABLE orderbook (exchange CHAR(20), bid FLOAT(10,4), ask FLOAT(10,4), diff_bid FLOAT(10,4), diff_ask FLOAT(10,4), timestamp DATETIME)")
+    except:
+        return False
 
 def get_orderbook():
-    exchange1 = ccxt.binance()
-    exchange2 = ccxt.kucoin()
+    myexchanges = ['binance', 'kucoin']
 
+    exchange1 = eval("ccxt." + myexchanges[0] + "()")
+    exchange2 = eval("ccxt." + myexchanges[1] + "()")
     orderbook1 = exchange1.fetch_order_book('BTC/USDT')
     orderbook2 = exchange2.fetch_order_book('BTC/USDT')
 
-    dict1 = orderbook1
-    highest_bid1 = dict1.get('bids')[0][0]
-    lowest_ask1 = dict1.get('asks')[0][0]
+    highest_bid1 = orderbook1.get('bids')[0][0]
+    lowest_ask1 = orderbook1.get('asks')[0][0]
 
-    dict2 = orderbook2
-    highest_bid2 = dict2.get('bids')[0][0]
-    lowest_ask2 = dict2.get('asks')[0][0]
+    highest_bid2 = orderbook2.get('bids')[0][0]
+    lowest_ask2 = orderbook2.get('asks')[0][0]
     
     diff_bid = highest_bid1 - highest_bid2
     diff_ask = lowest_ask1 - lowest_ask2
-    print(diff_bid)
-    save_data(f"{exchange1.id}", highest_bid1,lowest_ask1,f"{exchange2.id}",highest_bid1,lowest_ask2, diff_bid, diff_ask)
+    
+    save_data(f"{exchange1.id}", highest_bid1,lowest_ask1,f"{exchange2.id}",highest_bid2,lowest_ask2, diff_bid, diff_ask)
+    #print(f'Binance Bid: {highest_bid1}\n Binance Ask:{lowest_ask1}\n Kucoin Bid:{highest_bid2}\n Kucoin Ask: {lowest_ask2}')
 
 def save_data(exchange1,bid1,ask1,exchange2,bid2,ask2, diff_bid, diff_ask):
     mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="password",
+    user=f"{username}",
+    password=f"{password}",
     database="ccxtdatabase"
     )
-
+    
     mycursor = mydb.cursor()
-
     now = datetime.now()
     formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
     sql = "INSERT INTO orderbook (exchange, bid, ask, diff_bid, diff_ask, timestamp) VALUES (%s, %s, %s, %s,%s,%s)"
@@ -69,8 +75,10 @@ def save_data(exchange1,bid1,ask1,exchange2,bid2,ask2, diff_bid, diff_ask):
     print('saved')
     print(formatted_date)
     time.sleep(5)
-#create_database()
-#create_table()
+
+create_database()
+create_table()
+
 while True:
     get_orderbook()
 
--- display_data.py
+++ display_data.py
@@ -1,50 +1,118 @@
 import mysql.connector
 import pandas as pd
-import numpy as np
+import os
 import time
-import sys
+import credentials
 
+username = credentials.username
+password = credentials.password
+clear = lambda: os.system('clear')
 
-
-def display_data():
+def read_prices(interval):
     mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="password",
+    user=f"{username}",
+    password=f"{password}",
     database="ccxtdatabase"
     )
-
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT bid, ask, diff_bid, diff_ask, exchange  FROM orderbook WHERE timestamp >= DATE_SUB(NOW(),INTERVAL 15 MINUTE)")
+    #Exchange1
 
-    myresult = mycursor.fetchall()
-    print (myresult)
-#display_data()
+    mycursor.execute(f"SELECT bid,ask FROM orderbook WHERE exchange = 'binance'")
+    values = mycursor.fetchall()
+    bids1 = []
+    asks1 = []
+    for value in values:
+        
+        bids1.append(value[0])
+        asks1.append(value[1])
 
-def get_stats(interval):
+    try:
+        avg_bid1 = (sum(bids1)/len(bids1))
+        avg_ask1 = (sum(asks1)/len(asks1))
+    except:
+        avg_bid1, avg_ask1 = 'No entries  ', 'No entries  '  
+
+   #Exchange2
+    mycursor = mydb.cursor()
+    mycursor.execute(f"SELECT bid,ask FROM orderbook WHERE exchange = 'kucoin' AND timestamp >= DATE_SUB(NOW(),INTERVAL {interval} MINUTE)")
+    values = mycursor.fetchall()
+    bids2 = []
+    asks2 = []
+    for value in values:
+        bids2.append(value[0])
+        asks2.append(value[1])
+
+    try:
+        avg_bid2 = (sum(bids2)/len(bids2))
+        avg_ask2 = (sum(asks2)/len(asks2))
+    except:
+        avg_bid2, avg_ask2 = 'No entries  ', 'No entries  '
+    return avg_bid1, avg_ask1, avg_bid2, avg_ask2
+
+def read_diffs(interval):
     mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="password",
+    user=f"{username}",
+    password=f"{password}",
     database="ccxtdatabase"
     )
-
     mycursor = mydb.cursor()
-    mycursor.execute(f"SELECT diff_bid FROM orderbook WHERE timestamp >= DATE_SUB(NOW(),INTERVAL {interval} MINUTE)")
+    mycursor.execute(f"SELECT diff_bid, diff_ask FROM orderbook WHERE timestamp >= DATE_SUB(NOW(),INTERVAL {interval} MINUTE)")
+    values = mycursor.fetchall()
+    
+    diff_bids = []
+    diff_asks = []
+    for value in values:
+        diff_bids.append(value[0])
+        diff_asks.append(value[1])
+    if len(diff_bids) and len(diff_asks) != 0:
+        avg_diff_bid = (sum(diff_bids)/len(diff_bids))
+        max_diff_bid = max(diff_bids, key=abs)
+        avg_diff_ask = (sum(diff_asks)/len(diff_asks))
+        max_diff_ask = max(diff_asks, key=abs)        
 
-    result = mycursor.fetchall()
-    diff_bids_list = []
-    for item in result:
-        diff_bids_list.append(item[0])
-    if not len(diff_bids_list) == 0:
-        avg_diff_bid = (sum(diff_bids_list)/len(diff_bids_list))
-        max_diff_bid = max(diff_bids_list, key=abs)
-        print(max_diff_bid, end = "\r")
-        #print(max_diff_bid, flush=True)
     else:
-        avg_diff_bid, max_diff_bid = 'No entries'   
+        avg_diff_bid, max_diff_bid, avg_diff_ask, max_diff_ask = 'No entries  ', 'No entries  ', 'No entries  ', 'No entries  '
 
-    return avg_diff_bid, max_diff_bid
+    return avg_diff_bid, max_diff_bid, avg_diff_ask, max_diff_ask
+
+def calculate_percentages(interval):
+    avg_bid1, avg_ask1, avg_bid2, avg_ask2 = read_prices(interval)
+    avg_diff_bid, max_diff_bid, avg_diff_ask, max_diff_ask = read_diffs(interval)
+    try:
+        avg_diff_bid_percentage = (avg_diff_bid/avg_bid1)*100
+        max_diff_bid_percentage = (max_diff_bid/avg_bid1)*100
+        avg_diff_ask_percentage = (avg_diff_ask/avg_ask1)*100
+        max_diff_ask_percentage = (max_diff_ask/avg_ask1)*100
+    except:
+        avg_diff_bid_percentage, max_diff_bid_percentage, avg_diff_ask_percentage, max_diff_ask_percentage = 'No entries  ', 'No entries  ', 'No entries  ', 'No entries  '
+    return avg_diff_bid_percentage, max_diff_bid_percentage, avg_diff_ask_percentage, max_diff_ask_percentage
+
+def display_data(interval):
+    avg_bid1, avg_ask1, avg_bid2, avg_ask2 = read_prices(interval)
+    avg_diff_bid, max_diff_bid, avg_diff_ask, max_diff_ask = read_diffs(interval)
+    avg_diff_bid_percentage, max_diff_bid_percentage, avg_diff_ask_percentage, max_diff_ask_percentage = calculate_percentages(interval)
+    df = pd.DataFrame({
+    'Binance Average': [avg_bid1, avg_ask1],
+    'Kucoin Average': [avg_bid2, avg_ask2],
+    'Average Diff [$]': [avg_diff_bid, avg_diff_ask],
+    'Average Diff [%]': [avg_diff_bid_percentage, avg_diff_ask_percentage],
+    'Max Diff [$]': [max_diff_bid, max_diff_ask],
+    'Max Diff [%]': [max_diff_bid_percentage, max_diff_ask_percentage]
+
+    },
+    index=['Bids', 'Asks'])
+
+    df.columns=["Binance Average","Kucoin Average","Average Diff [$]", "Average Diff [%]", "Max Diff [$]", "Max Diff [%]"]
+    return df    
+UP = "\x1B[3A"
 
 while True:
-    get_stats(1)
+    df1=display_data(1)
+    df2=display_data(15)
+    df3=display_data(60)
+    df4=display_data(3600)
+    clear()
+    print(f'Last Minute \n{df1}\n\nLast 15 Minutes \n{df2}\n\nLast Hour \n{df3}\n\nLast Day \n{df4} ')
+    time.sleep(5)
